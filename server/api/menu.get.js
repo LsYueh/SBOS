24efb008c432595d8196297cd702a4e442c067f4
LsYueh <lsyueh@gmail.com>
2025-09-18 10:15:26 +0800

    更新選單

--- server/api/menu.get.js
+++ server/api/menu.get.js
@@ -13,23 +13,23 @@ export default defineEventHandler(() => {
     {
       title: '操作',
       items: [
-        { label: '成交回報上傳 (MHOK)', to: '/MHOK', roles: ['admin', 'user'] },
+        { label: '交易明細 (MFIO)', to: '/MFIO', roles: ['admin', 'user'] },
         { label: '成交明細匯總 (MHIO)', to: '/MHIO', roles: ['admin', 'user'] },
+        { label: '成交回報上傳 (MHOK)', to: '/MHOK', roles: ['admin', 'user'] },
       ]
     },
     {
       title: '報表',
       items: [
-        { label: '銷售報表', to: '/report/sales', roles: ['admin', 'manager'] },
-        { label: '庫存報表', to: '/report/inventory', roles: ['admin'] },
-        { label: '財務報表', to: '/report/finance', roles: ['admin', 'manager'] }
+        { label: '庫存日報表 (MSTF)', to: '/report/MSTF', roles: ['admin', 'manager'] },
       ]
     },
     {
       title: '管理',
       items: [
+        { label: '使用者設定 (TLB)', to: '/TLB', roles: ['admin'] },
         { label: '假日設定 (HLD)', to: '/HLD', roles: ['admin'] },
       ]
-    }
+    },
   ]
 })
