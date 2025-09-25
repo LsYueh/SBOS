e346214af1d617ea266e8dacef5ffd380c9d9467
LsYueh <lsyueh@gmail.com>
2025-09-25 15:28:18 +0800

    新增HLD的DDL

--- sql/19700101000000-init-schema/020-posts.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS sbos.posts (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v1(),
-  title VARCHAR(255) NOT NULL,
-  content TEXT,
-  created_at TIMESTAMP DEFAULT now(),
-  updated_at TIMESTAMP DEFAULT now()
-);
